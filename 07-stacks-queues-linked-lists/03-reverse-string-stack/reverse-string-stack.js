c898d88ba5249b108fbcee2dc4deb9757975248a
Vishesh Singh <visheshsingh16@gmail.com>
2025-09-17 13:40:11 -0400

    reverse a string using custom stack

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,16 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+  const stack = new Stack();
+  for (let i = 0; i < str.length; i++) {
+    stack.push(str[i]);
+  }
+  console.log(stack);
+  let revString = '';
+  while (!stack.isEmpty()) {
+    revString += stack.pop();
+  }
+  return revString;
+}
 
 module.exports = reverseStringStack;
