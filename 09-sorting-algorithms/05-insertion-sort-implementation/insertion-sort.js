ba726d1e8201a8da594f190b9e9b0dbf72d33ef1
Vishesh Singh <visheshsingh16@gmail.com>
2025-09-27 21:24:13 -0400

    insertion sort

--- 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
+++ 09-sorting-algorithms/05-insertion-sort-implementation/insertion-sort.js
@@ -1,3 +1,17 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1,
+      current = arr[i];
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+      console.log({ arr });
+    }
+    arr[j + 1] = current;
+    console.log('After swap:', arr);
+  }
+  console.log('What is arr: ', arr);
+  return arr;
+}
 
 module.exports = insertionSort;
