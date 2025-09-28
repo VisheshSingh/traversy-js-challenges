bc560c31a35588fee668b2e83d88b59d9d0d83d6
Vishesh Singh <visheshsingh16@gmail.com>
2025-09-27 21:24:07 -0400

    bubble sort

--- 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
+++ 09-sorting-algorithms/03-bubble-sort-implementation/bubble-sort.js
@@ -1,3 +1,16 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`Iteration: ${i + 1}`);
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+      console.log('What is arr', arr);
+    }
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
