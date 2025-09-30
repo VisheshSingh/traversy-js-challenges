f3a1de8bddc33fd2234cdb40a2d0a4eb34cdf4e8
Vishesh Singh <visheshsingh16@gmail.com>
2025-09-30 11:48:08 -0400

    add border

--- 100algos/addBorder/addBorder.ts
+++ 100algos/addBorder/addBorder.ts
@@ -1,5 +1,8 @@
 export function addBorder(picture: string[]): string[] {
-
+  const wall = '*'.repeat(picture[0].length + 2);
+  const items = picture.map((item) => '*' + item + '*');
+  return [wall, ...items, wall];
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(['abc', 'ded']));
+console.log(addBorder(['abcdeg', 'dedef']));
