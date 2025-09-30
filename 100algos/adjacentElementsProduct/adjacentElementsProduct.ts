f240fb143af64e01de4aeb90d529dc5c0518f3d4
Vishesh Singh <visheshsingh16@gmail.com>
2025-09-30 12:03:25 -0400

    adjacentElementsProduct

--- 100algos/adjacentElementsProduct/adjacentElementsProduct.ts
+++ 100algos/adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-
+  let largestProd = inputArray[0] * inputArray[1];
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] * inputArray[i + 1] > largestProd) {
+      largestProd = inputArray[i] * inputArray[i + 1];
+    }
+  }
+  return largestProd;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -10, -5, 7, 3]));
