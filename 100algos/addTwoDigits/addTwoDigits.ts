14bf47623feb98b960da2112149e69511ff22ad1
Vishesh Singh <visheshsingh16@gmail.com>
2025-09-30 11:53:30 -0400

    add two digits

--- 100algos/addTwoDigits/addTwoDigits.ts
+++ 100algos/addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,17 @@
 export function addTwoDigits(n: any): number {
-
+  const [n1, n2] = n.toString().split('');
+  return +n1 + +n2;
 }
 
-// console.log(addTwoDigits(29));
+export function addTwoDigits2(n: any): number {
+  const rem = n % 10;
+  const quotient = Math.floor(n / 10);
+  console.log(rem);
+  console.log(quotient);
+  return rem + quotient;
+}
+
+console.log(addTwoDigits(29));
+console.log(addTwoDigits2(29));
+console.log(addTwoDigits2(35));
+console.log(addTwoDigits2(78));
