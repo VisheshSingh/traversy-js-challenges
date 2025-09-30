d1ab96c77f04fcfda7ff73d33d732acc6cd953bc
Vishesh Singh <visheshsingh16@gmail.com>
2025-09-30 15:34:55 -0400

    alphabetSubsequence

--- 100algos/alphabetSubSequence/alphabetSubSequence.ts
+++ 100algos/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,20 @@
 export function alphabetSubsequence(s: string): boolean {
-
+  const chars = s.split('');
+  const charValues = [];
+  if (new Set(chars).size !== chars.length) {
+    return false;
+  }
+  for (let char of chars) {
+    charValues.push(char.charCodeAt(0));
+  }
+  for (let i = 0; i < charValues.length - 1; i++) {
+    if (charValues[i] > charValues[i + 1]) return false;
+  }
+  return true;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence('zab'));
+console.log(alphabetSubsequence('effg'));
+console.log(alphabetSubsequence('cdce'));
+console.log(alphabetSubsequence('ace'));
+console.log(alphabetSubsequence('bxz'));
