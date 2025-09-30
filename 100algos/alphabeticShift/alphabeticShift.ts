fad1e27940991d0a5dce53d6cb74b0a65bbdb27f
Vishesh Singh <visheshsingh16@gmail.com>
2025-09-30 15:25:45 -0400

    alphabeticShift

--- 100algos/alphabeticShift/alphabeticShift.ts
+++ 100algos/alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,13 @@
 export function alphabeticShift(inputString: string): string {
-
+  const chars = inputString.split('');
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i] === 'z') {
+      chars[i] = 'a';
+    } else {
+      chars[i] = String.fromCharCode(chars[i].charCodeAt(0) + 1);
+    }
+  }
+  return chars.join('');
 }
 
 console.log(alphabeticShift('crazy'));
