eaeb5225aab4bcd9264cedffa8db25ab3074b933
Vishesh Singh <visheshsingh16@gmail.com>
2025-09-30 15:02:04 -0400

    allLongestStrings

--- 100algos/allLongestStrings/allLongestStrings.ts
+++ 100algos/allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,13 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  let longestLength = 0;
+  inputArray.forEach((word) => {
+    if (word.length > longestLength) {
+      longestLength = word.length;
+    }
+  });
 
+  console.log(longestLength);
+  return inputArray.filter((word) => word.length === longestLength);
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba']));
