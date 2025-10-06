36aee5ce576e2de256e045f55b34b3b864402982
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-06 14:07:42 -0400

    arrayMaximalAdjacentDifference

--- 100algos/arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ 100algos/arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,10 @@
-export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-
+export function arrayMaximalAdjacentDifference(arr: number[]): number {
+  let max = Math.abs(arr[0] - arr[1]);
+  for (let i = 2; i < arr.length; i++) {
+    let absval = Math.abs(arr[i - 1] - arr[i]);
+    max = absval > max ? absval : max;
+  }
+  return max;
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
