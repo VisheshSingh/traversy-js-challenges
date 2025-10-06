da950223f7ee6e887a1fe3b0f8b0eb85b34a7b28
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-06 08:54:17 -0400

    are similar

--- 100algos/areSimilar/areSimilar.ts
+++ 100algos/areSimilar/areSimilar.ts
@@ -1,7 +1,21 @@
 export function areSimilar(a: number[], b: number[]): boolean {
+  if (a.length !== b.length) return false;
+  let diff = [];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      diff.push(i);
+      if (diff.length > 2) return false;
+    }
+  }
+  if (diff.length === 0) return true;
+  if (diff.length === 2) {
+    const [i, j] = diff;
+    return a[i] === b[j] && a[j] === b[i];
+  }
 
+  return false;
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
