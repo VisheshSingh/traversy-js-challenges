758f73b082b439c7598b863c471bfdc4fad55675
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-06 14:42:25 -0400

    array replace

--- 100algos/arrayReplace/arrayReplace.ts
+++ 100algos/arrayReplace/arrayReplace.ts
@@ -1,5 +1,14 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  inputArray.forEach((item, i) => {
+    if (item === elemToReplace) {
+      inputArray[i] = substitutionElem;
+    }
+  });
+  return inputArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
