3469bbb0196cb37d49c6acc918748a212b9187f1
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-06 15:18:43 -0400

    Bishop and Pawn ♟️

--- 100algos/bishopAndPawn/bishopAndPawn.ts
+++ 100algos/bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,23 @@
 export function bishopAndPawn(bishop: string, pawn: string): boolean {
+  let board = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+  };
 
+  const bishopX = board[bishop[0]];
+  const bishopY = +bishop[1];
+  const pawnX = board[pawn[0]];
+  const pawnY = +pawn[1];
+
+  return (
+    bishopX + bishopY === pawnX + pawnY || bishopX + pawnY === pawnX + bishopY
+  );
 }
 
-// console.log(bishopAndPawn('a1', 'c3'));
+console.log(bishopAndPawn('a1', 'c3'));
