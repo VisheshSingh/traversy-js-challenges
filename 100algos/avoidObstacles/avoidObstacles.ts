2c68e6ac266bcea718ba4fa870d4c3a7b3e22ca0
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-06 14:46:05 -0400

    avoidObstacles

--- 100algos/avoidObstacles/avoidObstacles.ts
+++ 100algos/avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,20 @@
-export function avoidObstacles(inputArray: number[]): number {
+export function avoidObstacles(arr: number[]): number {
+  let obstacles = arr.sort();
+  let max = Math.max(...obstacles);
 
+  for (let jump = 1; jump < max + 1; jump++) {
+    let hitObstacle = false;
+    for (let pos of obstacles) {
+      if (pos % jump === 0) {
+        hitObstacle = true;
+        break;
+      }
+    }
+
+    if (!hitObstacle) {
+      return jump;
+    }
+  }
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
