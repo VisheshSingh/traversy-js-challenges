83de54e1a33414d31c87f8d63a88d40af161e9d1
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-06 14:12:43 -0400

    arrayPreviousLess

--- 100algos/arrayPreviousLess/arrayPreviousLess.ts
+++ 100algos/arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,14 @@
 export function arrayPreviousLess(items: number[]): number[] {
-
+  let stack: number[] = [];
+  let result = Array.from({ length: items.length }).fill(-1);
+  for (let i = 0; i < items.length; i++) {
+    while (stack.length > 0 && stack[stack.length - 1] >= items[i]) {
+      stack.pop();
+    }
+    result[i] = stack.length === 0 ? -1 : stack[stack.length - 1];
+    stack.push(items[i]);
+  }
+  return result as number[];
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
