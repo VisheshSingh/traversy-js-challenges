4e97ae02704c4ebb933c51a1f07e88dfc03f7f63
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-06 08:49:24 -0400

    are equally strong

--- 100algos/areEquallyStrong/areEquallyStrong.ts
+++ 100algos/areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,21 @@
-export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+export function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+    const yourStrongest = Math.max(yourLeft, yourRight);
+    const yourWeakest = Math.min(yourLeft, yourRight);
+    const friendsStrongest = Math.max(friendsLeft, friendsRight);
+    const friendsWeakest = Math.min(friendsLeft, friendsRight);
+    console.log(yourStrongest)
+    console.log(yourWeakest)
+    console.log(friendsStrongest)
+    console.log(friendsWeakest);
 
+    return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
