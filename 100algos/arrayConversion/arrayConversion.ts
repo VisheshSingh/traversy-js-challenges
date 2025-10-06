f4f2f2288dbd574ba18eeff2cc9faa4a95c80d54
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-06 09:14:53 -0400

    array conversion

--- 100algos/arrayConversion/arrayConversion.ts
+++ 100algos/arrayConversion/arrayConversion.ts
@@ -1,5 +1,24 @@
 export function arrayConversion(inputArray: number[]): number {
-
+  let isOdd = false;
+  while (inputArray.length !== 1) {
+    inputArray = sumProduct(inputArray, isOdd);
+    isOdd = !isOdd;
+  }
+  return inputArray[0];
 }
 
-// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+function sumProduct(arr: number[], isOdd: boolean): number[] {
+  let result = [];
+  if (isOdd) {
+    for (let i = 0; i < arr.length; i += 2) {
+      result.push(arr[i] * arr[i + 1]);
+    }
+  } else {
+    for (let i = 0; i < arr.length; i += 2) {
+      result.push(arr[i] + arr[i + 1]);
+    }
+  }
+  return result;
+}
+
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
