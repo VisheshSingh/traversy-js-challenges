fa634e352a5712128645a624a54cfed885106231
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-06 09:01:03 -0400

    array change

--- 100algos/arrayChange/arrayChange.ts
+++ 100algos/arrayChange/arrayChange.ts
@@ -1,5 +1,13 @@
 export function arrayChange(inputArray: number[]): number {
-
+  let moves = 0;
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i] <= inputArray[i - 1]) {
+      let needed = inputArray[i - 1] - inputArray[i] + 1;
+      inputArray[i] += needed;
+      moves += needed;
+    }
+  }
+  return moves;
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
